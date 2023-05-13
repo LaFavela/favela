f2a41970bd1424fc879214dff80796f888b1f522
VosMovok <dimasnugraha46@gmail.com>
2023-05-13 21:20:23 +0800

    fixed import font-poppins

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <link
-          href="https://fonts.googleapis.com/css?family=Poppins"
-          rel="stylesheet"
-        ></link>
-      <body className='bg-white'>
+      <body className='bg-white {`${poppins.variable} font-sans`} text-black'>
         <Main />
         <NextScript />
       </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        poppins: ["Poppins"],
+        poppins: ['var(--font-poppins)'],
       },
     },
   },
