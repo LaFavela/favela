f23b287c26698c901c641467a9285f2aa7ef2e82
VosMovok <dimasnugraha46@gmail.com>
2023-06-04 21:17:56 +0800

    moved components to components folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Slider from "./Slider";
-import Footer from "./footer";
-import Navbar from "./navbar";
+import Footer from "../components/footer";
+import Navbar from "@/components/navbar";
 
 export default function Landing() {
    const [isPressed, setIsPressed] = useState(false);
