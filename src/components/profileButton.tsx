a5819a3b89872a4446e13a9e3553fb2e9679e39f
RamaAlqdri <ramadhanialqadri12@gmail.com>
2023-06-05 16:27:45 +0800

    add Pop Up Profile Nav and edit Profile Button

--- src/components/profileButton.tsx
+++ src/components/profileButton.tsx
@@ -1,12 +1,26 @@
+import ProfileNav from "./profileNav"
+import { useState } from "react";
+
+
 export default function ProfileButton(
     props: any = { transparent: false }
 ) {
+    const [showProfileNav, setShowProfileNav] = useState(false);
+    const handleOnClose = () => setShowProfileNav(false);
     let textColor = "text-black ";
     if (props.transparent) {
         textColor = "text-white ";
     }
   return (
-    <div>
+    <div
+      onClick={() => {
+        if (showProfileNav) {
+          setShowProfileNav(false);
+        }else {
+          setShowProfileNav(true)
+        }
+      }} 
+      className="cursor-pointer flex justify-end">
       <div className="flex items-center space-x-4">
         <img
           src="assets/profile/avatar.jpg"
@@ -16,6 +30,9 @@ export default function ProfileButton(
         <div>
           <p className={"text-[1.3125rem] font-light " + textColor}>Ramadhani</p>
         </div>
+      </div>
+      <div className="absolute right-5 mt-[4.3rem]">
+        <ProfileNav isAgent={true} isDesigner={true} onClose={handleOnClose} visible={showProfileNav}></ProfileNav>
       </div>
     </div>
   );
