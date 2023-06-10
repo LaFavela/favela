a1568ca138b95c546915a8576647ff89d6eb71f3
RamaAlqdri <ramadhanialqadri12@gmail.com>
2023-06-10 16:47:52 +0800

    tes

--- src/pages/aboutUs.tsx
+++ src/pages/aboutUs.tsx
@@ -1,5 +1,5 @@
-import Navbar from "./navbar";
-import Footer from "./footer";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 import Image from "next/image";
 
 const data = [
