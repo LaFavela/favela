edcab893c75fdb1cadaedc7e8f1b84d63ed227d8
RamaAlqdri <ramadhanialqadri12@gmail.com>
2023-06-12 19:37:53 +0800

    removad trailing comma

--- src/components/profileNav.tsx
+++ src/components/profileNav.tsx
@@ -1,4 +1,4 @@
-import React, { useState, } from "react";
+import React, { useState } from "react";
 import {motion, AnimatePresence} from "framer-motion";
 import Link from "next/link"
 import { signOut } from "next-auth/react";
