ea4dcddb6fc261fed156ecbdc702cd8c9dc00029
VosMovok <dimasnugraha46@gmail.com>
2023-06-12 18:29:45 +0800

    removed poppins from app as its not working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,11 @@
 import Providers from "@/components/Providers";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Poppins } from "next/font/google";
-
-const poppins = Poppins({
-   subsets: ["latin"],
-   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-   variable: "--font-poppins",
-});
 
 export default function App({ Component, pageProps }: AppProps) {
    return (
       <Providers>
-         <main className={`${poppins.variable}`}>
+         <main>
             <Component {...pageProps} />
          </main>
       </Providers>
