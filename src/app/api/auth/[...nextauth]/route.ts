afaff8de86abc0c40f723474ab10bc037c15fae4
VosMovok <dimasnugraha46@gmail.com>
2023-06-12 18:27:35 +0800

    finishing nextauth session, login, jwt

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,15 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma";
+
+
 const handler = NextAuth({
+   secret: process.env.NEXTAUTH_SECRET,
    providers: [
       CredentialsProvider({
          // The name to display on the sign in form (e.g. "Sign in with...")
-         name: "Credentials",
+         id: "username-login",
+         name: "credentials",
          // `credentials` is used to generate a form on the sign in page.
          // You can specify which fields should be submitted, by adding keys to the `credentials` object.
          // e.g. domain, username, password, 2FA token, etc.
@@ -16,42 +21,43 @@ const handler = NextAuth({
                placeholder: "jsmith",
             },
             password: { label: "Password", type: "password" },
+            rememberMe: { label: "Remember Me", type: "checkbox" },
          },
          async authorize(credentials, req) {
             // Add logic here to look up the user from the credentials supplied
-            const res = await fetch("http:/localhost:3000/api/login", {
+            const { username, password, rememberMe } = credentials as any;
+            const res = await fetch("http://localhost:3000/api/login", {
                method: "POST",
                headers: {
                   "Content-Type": "application/json",
                },
                body: JSON.stringify({
-                  username: credentials?.username,
-                  password: credentials?.password,
+                  username: username,
+                  password: password,
+                  rememberMe: rememberMe,
                }),
             });
 
             const user = await res.json();
 
-            if (user) {
-               // Any object returned will be saved in `user` property of the JWT
-               return user;
-            } else {
-               // If you return null then an error will be displayed advising the user to check their details.
-               return null;
 
-               // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-            }
+            if (res.ok && user) {
+               return user;
+            } else return null;
          },
       }),
    ],
    callbacks: {
       async jwt({ token, user }) {
-         return { ...token, ...user };
+         return { ...token, ...user};
       },
-
-      async session({ session, token }) {
+      async session({ session, token, user }) {
+         // Send properties to the client, like an access_token from a provider.
          session.user = token as any;
          return session;
+      },
+      async redirect({ url, baseUrl }) {
+         return url.startsWith(baseUrl) ? url : baseUrl;
       },
    },
 });
--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,26 +1,49 @@
+import { signJwtAccessToken } from "@/lib/jwt";
 import prisma from "@/lib/prisma";
 import * as bcrypt from "bcrypt";
+import { SignOptions } from "jsonwebtoken";
 
 interface RequestBody {
    username: string;
    password: string;
+   rememberMe: boolean;
 }
 
 export async function POST(request: Request) {
    const body: RequestBody = await request.json();
 
-   const user = await prisma.users.findUnique({
+   const user = await prisma.users.findFirst({
       where: {
-         email: body.username,
+         OR: [
+            {
+               username: body.username,
+            },
+            {
+               email: body.username,
+            },
+         ],
       },
    });
 
-   if (!user) {
-   }
-
    if (user && (await bcrypt.compare(body.password, user.password))) {
       const { password, ...userWithoutPassword } = user;
-      return new Response(JSON.stringify(userWithoutPassword));
+      
+      let expiresIn: SignOptions = { expiresIn: "1h" };
+      if (body.rememberMe) {
+         expiresIn = {
+            expiresIn: "30d",
+         };
+      }
+      const accessToken = signJwtAccessToken(
+         userWithoutPassword,
+         expiresIn as any
+      );
+      const result = {
+         ...userWithoutPassword,
+         accessToken,
+         expiresIn: expiresIn.expiresIn,
+      };
+      return new Response(JSON.stringify(result));
    } else {
       return new Response(
          JSON.stringify({ error: "Invalid username or password" }),
@@ -30,4 +53,4 @@ export async function POST(request: Request) {
          }
       );
    }
-};
+}
