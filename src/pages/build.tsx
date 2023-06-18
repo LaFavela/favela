7b3409e2f193dc5a6c37be6d9ec71729ffb789ae
RamaAlqdri <ramadhanialqadri12@gmail.com>
2023-06-18 19:21:21 +0800

    fix build page

--- src/pages/build.tsx
+++ src/pages/build.tsx
@@ -370,7 +370,7 @@ export default function Build() {
         </p>
       </div>
       <div>
-        <div className="sticky top-20 z-30 flex justify-between bg-white px-16 py-5">
+        <div className="sticky top-20 z-30 flex justify-start space-x-10 bg-white px-16 py-5">
           <div className="ml-[5.5rem] flex space-x-3">
             <button
               onClick={() => handleFunctionChange("Design")}
