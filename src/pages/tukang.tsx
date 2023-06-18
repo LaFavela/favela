a14ee9ce3dda87501a22affd4329fe400d094b08
DzulhiRaihan <dzulhiraihan@gmail.com>
2023-06-19 00:07:29 +0800

    tukang-fix

--- src/pages/tukang.tsx
+++ src/pages/tukang.tsx
@@ -1,36 +1,31 @@
+import { Fragment } from "react";
 import { Listbox, Transition } from "@headlessui/react";
-import { useState, useRef, Fragment } from "react";
+import { useState, useRef, useEffect } from "react";
 import Image from "next/image";
 import Footer from "@/components/footer";
+import Link from "next/link";
+import merchantProfile from "./merchantProfile";
 
-type Data = {
-  id: number;
-  name: string;
+type Person = {
+   id: number;
+   name: string;
 };
-
-const type: Data[] = [
-  { id: 1, name: "Semua" },
-  { id: 2, name: "Rumah" },
-  { id: 3, name: "Kondominum" },
-  { id: 4, name: "Tanah" },
-  { id: 5, name: "Villa" },
+const people: Person[] = [
+   { id: 1, name: "Nama" },
+   { id: 2, name: "Popularitas" },
+   { id: 3, name: "Sering Dicari" },
 ];
 
-const filter: Data[] = [
-  { id: 1, name: "Nama" },
-  { id: 2, name: "Popularitas" },
-  { id: 3, name: "Sering Dicari" },
-];
-
-export const designerData = [
+export const data = [
   {
     id: 1,
     nama: "Ali An Nuur",
     img: "/assets/profile/ali.jpg",
-    designerName: "Rama",
+    designerName: "Ali",
     rating: 4,
     like: 12,
     job: "Ikea Designer",
+    
   },
   {
     id: 2,
@@ -45,7 +40,7 @@ export const designerData = [
     id: 3,
     nama: "Yan Saputra",
     img: "/assets/profile/yan.jpg",
-    designerName: "Yan",
+    designerName: "Freya",
     rating: 3,
     like: 12,
     job: "Ikea Designer",
@@ -68,109 +63,52 @@ export const designerData = [
     like: 12,
     job: "Ikea Designer",
   },
-];
-
-export function Dropdown() {
-  const [selected, setSelected] = useState<Data | null>(null);
-  const [isOpen, setIsOpen] = useState(false);
-  const [isClick, setIsClick] = useState(false);
-  const toggleDropdown = () => {
-    setIsClick(!isClick);
-  };
-
-  const refCategory = useRef<any>(null);
-
-  return (
-    <div className="flex gap-6 pr-[28rem]">
-      <div>
-        <form className="h-12" action="">
-          <div className="float-right pr-12"></div>
-          <input
-            type="text"
-            className="h-full w-full rounded-full bg-[#F1F1F1] pl-5 text-xl font-light focus:outline-none"
-            placeholder="Nama"
-          ></input>
-        </form>
-      </div>
-      <div className="flex flex-row space-x-3">
-        <div className="h-12 w-56">
-          <Listbox value={selected} onChange={setSelected}>
-            <Listbox.Button
-              onClick={toggleDropdown}
-              className={`h-full w-full cursor-default rounded-3xl py-2 pl-3 pr-10 text-left font-light sm:text-xl ${
-                isClick
-                  ? "bg-[#E4D1BC] text-[#B17C3F]"
-                  : "bg-[#F1F1F1] text-[#9ca3af]"
-              }`}
-            >
-              <div className="flex w-max">
-                <span className="w-[11rem]">
-                  {selected ? selected.name : "Choose Type"}
-                </span>
-                <span className="flex items-center justify-end ">
-                  {isClick ? (
-                    <svg
-                      width="19"
-                      height="11"
-                      viewBox="0 0 19 11"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="fill-[#B17C3F]"
-                    >
-                      <path d="M0.374609 0.374999C0.624609 0.124999 0.920609 -7.89165e-07 1.26261 -7.74216e-07C1.60461 -7.59267e-07 1.90028 0.124999 2.14961 0.374999L9.47461 7.7L16.8246 0.35C17.0579 0.116666 17.3496 -7.1031e-08 17.6996 -5.5732e-08C18.0496 -4.0433e-08 18.3496 0.125 18.5996 0.375C18.8496 0.625 18.9746 0.921 18.9746 1.263C18.9746 1.605 18.8496 1.90067 18.5996 2.15L10.1746 10.55C10.0746 10.65 9.96628 10.721 9.84961 10.763C9.73294 10.805 9.60794 10.8257 9.47461 10.825C9.34128 10.825 9.21628 10.804 9.09961 10.762C8.98294 10.72 8.87461 10.6493 8.77461 10.55L0.349609 2.125C0.116276 1.89167 -0.000391077 1.604 -0.000391062 1.262C-0.000391047 0.919999 0.124609 0.624332 0.374609 0.374999Z" />
-                    </svg>
-                  ) : (
-                    <svg
-                      width="19"
-                      height="11"
-                      viewBox="0 0 19 11"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="fill-slate-500"
-                    >
-                      <path d="M18.6 10.4501C18.35 10.7001 18.054 10.8251 17.712 10.8251C17.37 10.8251 17.0743 10.7001 16.825 10.4501L9.5 3.12507L2.15 10.4751C1.91667 10.7084 1.625 10.8251 1.275 10.8251C0.925 10.8251 0.625 10.7001 0.375 10.4501C0.125 10.2001 1.09828e-08 9.90407 1.50611e-08 9.56207C1.91394e-08 9.22007 0.125 8.92441 0.375 8.67507L8.8 0.275072C8.9 0.175072 9.00833 0.104073 9.125 0.0620729C9.24167 0.020073 9.36667 -0.000594027 9.5 7.25925e-05C9.63333 7.25941e-05 9.75833 0.0210725 9.875 0.0630723C9.99167 0.105072 10.1 0.175738 10.2 0.275072L18.625 8.70007C18.8583 8.93341 18.975 9.22107 18.975 9.56307C18.975 9.90507 18.85 10.2007 18.6 10.4501Z" />
-                    </svg>
-                  )}
-                </span>
-              </div>
-            </Listbox.Button>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Listbox.Options
-                onClick={toggleDropdown}
-                className="mt-2 max-h-max w-full overflow-auto rounded-xl bg-white py-1 text-xl shadow-sm ring-1 ring-black ring-opacity-5"
-                ref={refCategory}
-              >
-                {filter.map((person) => (
-                  <Listbox.Option
-                    key={person.id}
-                    value={person}
-                    className={({ active }) =>
-                      `relative mx-2 cursor-pointer select-none rounded-xl px-4 py-2 ${
-                        active
-                          ? "bg-[#E8D6C2] text-[#B17C3F]"
-                          : "text-[#9ca3af]"
-                      }`
-                    }
-                  >
-                    {person.name}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </Listbox>
-        </div>
-      </div>
-    </div>
-  );
-}
+  {
+    id: 6,
+    nama: "Ali An Nuur",
+    img: "/assets/profile/ali.jpg",
+    designerName: "Ali",
+    rating: 4,
+    like: 12,
+    job: "Ikea Designer",
+  },
+  {
+    id: 7,
+    nama: "Dhira Wahyu Febrian",
+    img: "/assets/profile/dhira.jpg",
+    designerName: "Dhira",
+    rating: 4,
+    like: 12,
+    job: "Ikea Designer",
+  },
+  {
+    id: 8,
+    nama: "Yan Saputra",
+    img: "/assets/profile/yan.jpg",
+    designerName: "Freya",
+    rating: 3,
+    like: 12,
+    job: "Ikea Designer",
+  },
+  {
+    id: 9,
+    nama: "Muhammad Rezky Sulihin",
+    img: "/assets/profile/eky.jpg",
+    designerName: "Eky",
+    rating: 5,
+    like: 12,
+    job: "Ikea Designer",
+  },
+  {
+    id: 10,
+    nama: "Muhammad Dwimas Catur N.",
+    img: "/assets/profile/dwimas.jpg",
+    designerName: "Dwimas",
+    rating: 5,
+    like: 12,
+    job: "Ikea Designer",
+  },
+]
 
 export function generateRatingStars(rating: number) {
   const maxRating = 5;
@@ -188,7 +126,7 @@ export function generateRatingStars(rating: number) {
   return fullStars + emptyStars;
 }
 
-export default function Designer() {
+export function PilihTukang() {
   const [hover, setHover] = useState(false);
   const [index, setIndex] = useState(-1);
   const [isPressed, setIsPressed] = useState(false);
@@ -204,12 +142,10 @@ export default function Designer() {
 
   return (
     <div>
-      <div className=" ml-20 pt-10">
-        <div className="ml-[9rem] mt-6">
-          <Dropdown></Dropdown>
-        </div>
-        <div className="z-0 px-35 flex flex-grow flex-row flex-wrap justify-center gap-5">
-          {designerData.slice(0, visibleItems).map((designerData, idx) => {
+      <div className="ml-20 pt-10">
+        <Link href={"./merchantProfile"}>
+        <div className="px-35 flex flex-grow flex-row flex-wrap justify-center gap-5">
+          {data.slice(0, visibleItems).map((data, idx) => {
             return (
               <div
                 key={idx}
@@ -230,7 +166,7 @@ export default function Designer() {
                 <div className="relative h-full w-full flex-auto">
                   <Image
                     className="rounded-3xl"
-                    src={designerData.img}
+                    src={data.img}
                     alt={""}
                     fill={true}
                     style={{ objectFit: "cover" }}
@@ -242,7 +178,7 @@ export default function Designer() {
                         : "absolute top-3 ml-5 h-full w-full text-[1.3125rem] font-medium text-white"
                     }`}
                   >
-                    {designerData.designerName}
+                    {data.designerName}
                   </p>
                 </div>
                 <div
@@ -261,24 +197,24 @@ export default function Designer() {
                 >
                   <span className="flex flex-col space-y-3">
                     <div className="text-xl font-medium ">
-                      {designerData.nama}
+                      {data.nama}
                     </div>
 
                     <div className="flex flex-col">
                       {/* isi rating */}
                       <div
                         dangerouslySetInnerHTML={{
-                          __html: generateRatingStars(designerData.rating),
+                          __html: generateRatingStars(data.rating),
                         }}
                         style={{ display: "flex", flexDirection: "row" }}
                       ></div>
 
-                      <span>{designerData.job}</span>
+                      <span>{data.job}</span>
                     </div>
                   </span>
                   <span className="flex text-xl font-semibold">
                     <span className="text-[1.125rem] text-black">
-                      {designerData.like}
+                      {data.like}
                     </span>
                     <div className="ml-1 mr-3">
                       <button onClick={handleButtonClick}>
@@ -305,9 +241,11 @@ export default function Designer() {
             );
           })}
         </div>
+          
+        </Link>
       </div>
       <div className="ml-20 mt-10 flex items-center justify-center">
-        {visibleItems < designerData.length && (
+        {visibleItems < data.length && (
           <button
             className=" h-12 w-36 rounded-3xl border-2 border-[#B17C3F] bg-white text-xl text-[#B17C3F] duration-300 ease-in-out hover:border-[#d9b285] hover:bg-[#d9b285] hover:text-white"
             onClick={loadMore}
@@ -316,7 +254,138 @@ export default function Designer() {
           </button>
         )}
       </div>
-      <Footer></Footer>
     </div>
   );
+}
+
+
+
+export default function Tukang() {
+   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+   const [isOpen, setIsOpen] = useState(false);
+   const [isClick, setIsClick] = useState(false);
+   const toggleDropdown = () => {
+      setIsClick(!isClick);
+   };
+  const refCategory = useRef<any>(null);
+
+   function OutsideCategory(ref: any) {
+      useEffect(() => {
+         /**
+          * Alert if clicked on outside of element
+          */
+         function handleClickOutside(event: any) {
+            if (ref.current && !ref.current.contains(event.target)) {
+               if (!isClick) setIsClick(false);
+            }
+         }
+         // Bind the event listener
+         document.addEventListener("mousedown", handleClickOutside);
+         return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+         };
+      }, [ref]);
+   }
+
+   OutsideCategory(refCategory);
+
+   return (
+      <div>
+         <div className="ml-20 sticky top-20 z-30 flex space-x-10 bg-white px-16 py-5">
+            <div className="flex flex-row space-x-3">
+               <div className="h-12 w-56">
+                  <Listbox value={selectedPerson} onChange={setSelectedPerson}>
+                     <Listbox.Button
+                        onClick={toggleDropdown}
+                        className={`h-full w-full cursor-default rounded-3xl py-2 pl-3 pr-10 text-left font-light sm:text-xl ${
+                           isClick
+                              ? "bg-[#E4D1BC] text-[#B17C3F]"
+                              : "bg-[#F1F1F1] text-[#9ca3af]"
+                        }`}
+                     >
+                        <div className="flex w-max">
+                           <span className="w-[11rem]">
+                              {selectedPerson
+                                 ? selectedPerson.name
+                                 : "Choose Type"}
+                           </span>
+                           <span className="flex items-center justify-end ">
+                              {isClick ? (
+                                 <svg
+                                    width="19"
+                                    height="11"
+                                    viewBox="0 0 19 11"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="fill-[#B17C3F]"
+                                 >
+                                    <path d="M0.374609 0.374999C0.624609 0.124999 0.920609 -7.89165e-07 1.26261 -7.74216e-07C1.60461 -7.59267e-07 1.90028 0.124999 2.14961 0.374999L9.47461 7.7L16.8246 0.35C17.0579 0.116666 17.3496 -7.1031e-08 17.6996 -5.5732e-08C18.0496 -4.0433e-08 18.3496 0.125 18.5996 0.375C18.8496 0.625 18.9746 0.921 18.9746 1.263C18.9746 1.605 18.8496 1.90067 18.5996 2.15L10.1746 10.55C10.0746 10.65 9.96628 10.721 9.84961 10.763C9.73294 10.805 9.60794 10.8257 9.47461 10.825C9.34128 10.825 9.21628 10.804 9.09961 10.762C8.98294 10.72 8.87461 10.6493 8.77461 10.55L0.349609 2.125C0.116276 1.89167 -0.000391077 1.604 -0.000391062 1.262C-0.000391047 0.919999 0.124609 0.624332 0.374609 0.374999Z" />
+                                 </svg>
+                              ) : (
+                                 <svg
+                                    width="19"
+                                    height="11"
+                                    viewBox="0 0 19 11"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="fill-slate-500"
+                                 >
+                                    <path d="M18.6 10.4501C18.35 10.7001 18.054 10.8251 17.712 10.8251C17.37 10.8251 17.0743 10.7001 16.825 10.4501L9.5 3.12507L2.15 10.4751C1.91667 10.7084 1.625 10.8251 1.275 10.8251C0.925 10.8251 0.625 10.7001 0.375 10.4501C0.125 10.2001 1.09828e-08 9.90407 1.50611e-08 9.56207C1.91394e-08 9.22007 0.125 8.92441 0.375 8.67507L8.8 0.275072C8.9 0.175072 9.00833 0.104073 9.125 0.0620729C9.24167 0.020073 9.36667 -0.000594027 9.5 7.25925e-05C9.63333 7.25941e-05 9.75833 0.0210725 9.875 0.0630723C9.99167 0.105072 10.1 0.175738 10.2 0.275072L18.625 8.70007C18.8583 8.93341 18.975 9.22107 18.975 9.56307C18.975 9.90507 18.85 10.2007 18.6 10.4501Z" />
+                                 </svg>
+                              )}
+                           </span>
+                        </div>
+                     </Listbox.Button>
+                     <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                     >
+                        <Listbox.Options
+                           onClick={toggleDropdown}
+                           className="mt-2 max-h-max w-full overflow-auto rounded-xl bg-white py-1 text-xl shadow-sm ring-1 ring-black ring-opacity-5"
+                           ref={refCategory}
+                        >
+                           {people.map((person) => (
+                              <Listbox.Option
+                                 key={person.id}
+                                 value={person}
+                                 className={({ active }) =>
+                                    `relative mx-2 cursor-pointer select-none rounded-xl px-4 py-2 ${
+                                       active
+                                          ? "bg-[#E8D6C2] text-[#B17C3F]"
+                                          : "text-[#9ca3af]"
+                                    }`
+                                 }
+                              >
+                                 {person.name}
+                              </Listbox.Option>
+                           ))}
+                        </Listbox.Options>
+                     </Transition>
+                  </Listbox>
+               </div>
+               <div>
+                  <form className="h-12" action="">
+                     <div className="float-right pr-12"></div>
+                     <input
+                        type="text"
+                        className="h-full w-full rounded-full bg-[#F1F1F1] pl-5 text-xl font-light focus:outline-none"
+                        placeholder="Lokasi..."
+                     ></input>
+                  </form>
+               </div>
+            </div>
+         </div>
+         {/* batas */}
+         <PilihTukang></PilihTukang>
+        
+         <Footer></Footer>
+      </div>
+   );
 }
