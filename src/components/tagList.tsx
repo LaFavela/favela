aef4c7216277f83645c3d2471f24fd1697c971f2
DzulhiRaihan <dzulhiraihan@gmail.com>
2023-08-21 18:45:52 +0800

    kelupaan list buat tagnya

---
+++ src/components/tagList.tsx
@@ -0,0 +1,84 @@
+export const itemList = [
+  {
+    label: "type 1",
+    value: "type 1",
+    subListItem: [
+      
+      { label: "Style 1", value: "Style 1" },
+      { label: "Style 2", value: "Style 2" },
+      { label: "Style 3", value: "Style 3" },
+      { label: "Style 4", value: "Style 4" },
+      { label: "Style 5", value: "Style 5" },
+      { label: "Style 6", value: "Style 6" },
+    ],
+  },
+  {
+   label: "type 2",
+   value: "type 2",
+   subListItem: [
+     
+     { label: "Gaya 1", value: "Gaya 1" },
+     { label: "Gaya 2", value: "Gaya 2" },
+     { label: "Gaya 3", value: "Gaya 3" },
+     { label: "Gaya 4", value: "Gaya 4" },
+     { label: "Gaya 5", value: "Gaya 5" },
+     { label: "Gaya 6", value: "Style 6" },
+   ],
+ },
+ 
+ {
+   label: "type 3",
+   value: "type 3",
+   subListItem: [
+     
+     { label: "Model 1", value: "Model 1" },
+     { label: "Model 2", value: "Model 2" },
+     { label: "Model 3", value: "Model 3" },
+     { label: "Model 4", value: "Model 4" },
+     { label: "Model 5", value: "Model 5" },
+     { label: "Model 6", value: "Model 6" },
+   ],
+ },
+ 
+ {
+   label: "type 4",
+   value: "type 4",
+   subListItem: [
+     
+     { label: "Tone 1", value: "Tone 1" },
+     { label: "Tone 2", value: "Tone 2" },
+     { label: "Tone 3", value: "Tone 3" },
+     { label: "Tone 4", value: "Tone 4" },
+     { label: "Tone 5", value: "Tone 5" },
+     { label: "Tone 6", value: "Tone 6" },
+   ],
+ },
+ 
+ {
+   label: "type 5",
+   value: "type 5",
+   subListItem: [
+     
+     { label: "Shape 1", value: "Shape 1" },
+     { label: "Shape 2", value: "Shape 2" },
+     { label: "Shape 3", value: "Shape 3" },
+     { label: "Shape 4", value: "Shape 4" },
+     { label: "Shape 5", value: "Shape 5" },
+     { label: "Shape 6", value: "Shape 6" },
+   ],
+ },
+ 
+ {
+   label: "type 6",
+   value: "type 6",
+   subListItem: [
+     
+     { label: "Design 1", value: "Design 1" },
+     { label: "Design 2", value: "Design 2" },
+     { label: "Design 3", value: "Design 3" },
+     { label: "Design 4", value: "Design 4" },
+     { label: "Design 5", value: "Design 5" },
+     { label: "Design 6", value: "Design 6" },
+   ],
+ },
+];
