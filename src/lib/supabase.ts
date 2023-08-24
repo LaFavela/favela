b6a31c9d8f44d1a8c1f4c3505121af2f56156905
VosMovok <dimasnugraha46@gmail.com>
2023-08-25 01:24:28 +0800

    added types for type checking

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/types";
 
 const supabaseUrl = "https://jexluficpazfxobksszu.supabase.co";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey as string);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey as string);
