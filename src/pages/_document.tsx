8a3439736221ad29e7a9c20b85108f081374bc6b
VosMovok <dimasnugraha46@gmail.com>
2023-08-25 10:49:57 +0800

    added back css style to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
    return (
       <Html lang="en">
          <Head />
-         <body className="">
+         <body className="text-black bg-white">
             <Providers>
                <Main />
                <NextScript />
