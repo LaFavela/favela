2904b389f44932c5cb9a08c919515783032c7490
VosMovok <dimasnugraha46@gmail.com>
2023-09-09 11:44:34 +0800

    done transaction list

--- src/pages/transaction.tsx
+++ src/pages/transaction.tsx
@@ -1,93 +1,98 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { Skeleton } from "@mui/material";
+import { supabase } from "@/lib/supabase";
+import { User } from "@supabase/supabase-js";
+import { Database } from "@/types";
 const people = [
 	{ id: 1, name: "Semua" },
 	{ id: 2, name: "Property" },
 	{ id: 3, name: "Design" },
 	{ id: 4, name: "Build" },
 ];
-const data = [
-	{
-		id: "HNF29125367",
-		transactionType: "Build",
-		transactionDate: "16 January 2023",
-		propertyName: "Ampenan Asri",
-		img: "/assets/browse/Build14.png",
-		propertyType: "Villa",
-		price: 476000000,
-		status: "PENDING",
-	},
-	{
-		id: "HNF29125367",
 
-		transactionType: "Build",
-		transactionDate: "16 January 2023",
+type Transactions =
+	Database["public"]["Tables"]["transaction_contributor"]["Row"] & {
+		transaction_id: Database["public"]["Tables"]["transaction"]["Row"];
+	};
 
-		propertyName: "Perumahan Bumi Asri",
-		img: "/assets/browse/Build13.png",
-		propertyType: "Villa",
-		price: 476000000,
-		status: "FINISHED",
-	},
-	{
-		id: "HNF29125367",
-
-		transactionType: "Build",
-		transactionDate: "16 January 2023",
-
-		propertyName: "BTN Ali",
-		img: "/assets/browse/Build9.png",
-		propertyType: "Rumah",
-		price: 476000000,
-		status: "PENDING",
-	},
-];
 export default function Transaction() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-  
-  return (
+	const [transactionData, setTransactionData] = useState<Transactions[]>();
+	useEffect(() => {
+		const fetch = async () => {
+			const session = (await supabase.auth.getUser()).data.user;
+			const { data, error } = await supabase
+				.from("transaction_contributor")
+				.select("*, transaction_id(*)")
+				.or(
+					`client_id.eq.${session?.id},contractor_id.eq.${session?.id},designer_id.eq.${session?.id}`,
+				)
+				.returns<Transactions[]>();
+			if (data) setTransactionData(data);
+			console.log(data, error);
+		};
+		fetch();
+	}, []);
+
+	const router = useRouter();
+	const [loading, setLoading] = useState(true);
+	const [active, setActive] = useState(1);
+
+	return (
 		<div className="flex justify-center">
 			<div className="mt-[2.1875rem] w-[81.875rem] space-y-[1.3125rem]">
 				<div>
 					<p className="text-[1.25rem] font-medium">Transaction</p>
 				</div>
 				<div className="w-[55.5625rem] space-y-[1.125rem] p-[1.93rem] bg-white drop-shadow-landingShado  rounded-[1.5625rem]">
-					{data.length > 0 ? (
-						data.map((item, idx) => (
+					{transactionData && transactionData.length > 0 ? (
+						transactionData.map((item, idx) => (
 							<div
 								key={idx}
 								className="w-full h-fit p-[1.375rem] border border-[#e3d0ba] bg-white space-y-[0.875rem] rounded-[0.9375rem]"
 							>
 								<div className="flex space-x-3 items-center">
 									<p className="text-[0.875rem] font-semibold">
-										{item.transactionType}
+										{item.transaction_id.type}
 									</p>
-									<p className="text-[0.75rem]">{item.transactionDate}</p>
+									<p className="text-[0.75rem]">
+										{item.transaction_id.created_at}
+									</p>
 									<p className="text-[0.75rem]">{item.id}</p>
 								</div>
 								<div className="flex space-x-3">
 									<div className="relative w-[4.4375rem] h-[4.4375rem] rounded-[0.625rem] overflow-hidden">
-										<Image src={item.img} alt="property" fill={true} />
+										<Image
+											src={item.transaction_id.img}
+											alt="property"
+											fill={true}
+										/>
 									</div>
 									<div className="flex-col justify-between flex w-[21.5625rem]">
 										<p className="text-[0.875rem] font-light">
-											{item.propertyName}
+											{item.transaction_id.name}
 										</p>
 										<p className="text-[0.875rem] font-light text-[#BEB8B8]">
-											{item.propertyType}
+											{item.transaction_id.property_type}
 										</p>
 										<p className="text-[1rem] font-medium">
-											Rp. {item.price.toLocaleString("en-US")}.00
+											Rp.{" "}
+											{item.transaction_id.price_estimated.toLocaleString(
+												"en-US",
+											)}
+											.00
 										</p>
 									</div>
 									<div className="flex-col justify-between flex w-[21.5625rem]">
 										<p className="text-[0.875rem] font-semibold">Status:</p>
-										<p className="text-[0.75rem] font-light ">{item.status}</p>
-										<Link href={"/transaction"}>
+										<p className="text-[0.75rem] font-light ">
+											{item.transaction_id.status}
+										</p>
+										<Link
+											href={`/transaction/detail/${item.transaction_id.id}`}
+										>
 											<p className="text-[0.875rem] font-medium text-[#B17C3F]">
 												Detail Transaction
 											</p>
