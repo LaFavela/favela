9526095444b3a74589d1bdd51b497642c630b346
VosMovok <dimasnugraha46@gmail.com>
2023-09-13 04:21:25 +0700

    udpated_types

--- src/pages/transaction/index.tsx
+++ src/pages/transaction/index.tsx
@@ -18,6 +18,8 @@ type Transactions =
 		transaction_id: Database["public"]["Tables"]["transaction"]["Row"];
 	};
 
+	
+
 export default function Transaction() {
 	const [transactionData, setTransactionData] = useState<Transactions[]>();
 	useEffect(() => {
@@ -34,7 +36,6 @@ export default function Transaction() {
 		};
 		fetch();
 	}, []);
-
 	const router = useRouter();
 	const [loading, setLoading] = useState(true);
 	const [active, setActive] = useState(1);
@@ -52,45 +53,50 @@ export default function Transaction() {
 								key={idx}
 								className="w-full h-fit p-[1.375rem] border border-[#f7e6d4] bg-white cursor-pointer hover:bg-[#f7e6d4] space-y-[0.875rem] rounded-[0.9375rem]"
 							>
-								<Link href={`/transaction/detail/${item.transaction_id.id}`} className=" space-y-[0.875rem]">
-								<div className="flex space-x-3 items-center">
-									<p className="text-[0.875rem] font-semibold text-gold">
-										{item.transaction_id.property_type}
-									</p>
-									<p className="text-[0.75rem]">
-										{getDate(item.transaction_id.created_at)}
-									</p>
-									<p className="font-extralight text-gold">|</p>
-									<p className="text-[0.75rem]">{item.id}</p>
-								</div>
-								<div className="flex space-x-3">
-									<div className="relative w-[4.4375rem] h-[4.4375rem] rounded-[0.625rem] overflow-hidden">
-										<Image
-											src={item.transaction_id.img}
-											alt="property"
-											fill={true}
-										/>
+								<Link
+									href={`/transaction/detail/${item.transaction_id.id}`}
+									className=" space-y-[0.875rem]"
+								>
+									<div className="flex space-x-3 items-center">
+										<p className="text-[0.875rem] font-semibold text-gold">
+											{item.transaction_id.type}
+										</p>
+										<p className="text-[0.75rem]">
+											{getDate(item.transaction_id.created_at)}
+										</p>
+										<p className="font-extralight text-gold">|</p>
+										<p className="text-[0.75rem]">{item.id}</p>
 									</div>
-									<div className="flex-col justify-between flex w-[21.5625rem]">
-										<p className="text-[0.875rem] font-light">
-											{item.transaction_id.name}
-										</p>
-										<p className="text-[0.875rem] font-light text-[#BEB8B8]">
-											{item.transaction_id.property_type}
-										</p>
-										<p className="text-[1rem] font-semibold">
-											Rp
-											{item.transaction_id.price_estimated.toLocaleString(
-												"id-ID",
-											)}
-											,00
-										</p>
-									</div>
-									<div className="flex-col justify-start flex w-[21.5625rem]">
-										<p className="text-[0.875rem] font-semibold text-gold">Status:</p>
-										<p className="text-[0.75rem] font-light ">
-											{item.transaction_id.status}
-										</p>
+									<div className="flex space-x-3">
+										<div className="relative w-[4.4375rem] h-[4.4375rem] rounded-[0.625rem] overflow-hidden">
+											<Image
+												src={item.transaction_id.img}
+												alt="property"
+												fill={true}
+											/>
+										</div>
+										<div className="flex-col justify-between flex w-[21.5625rem]">
+											<p className="text-[0.875rem] font-light">
+												{item.transaction_id.name}
+											</p>
+											<p className="text-[0.875rem] font-light text-[#BEB8B8]">
+												{item.transaction_id.property_type}
+											</p>
+											<p className="text-[1rem] font-semibold">
+												Rp
+												{item.transaction_id.price_estimated.toLocaleString(
+													"id-ID",
+												)}
+												,00
+											</p>
+										</div>
+										<div className="flex-col justify-start flex w-[21.5625rem]">
+											<p className="text-[0.875rem] font-semibold text-gold">
+												Status:
+											</p>
+											<p className="text-[0.75rem] font-light ">
+												{item.transaction_id.id}
+											</p>
 										</div>
 									</div>
 								</Link>
@@ -108,3 +114,7 @@ export default function Transaction() {
 		</div>
 	);
 }
+function useMemo(arg0: () => string[] | undefined, arg1: (Transactions[] | undefined)[]) {
+	throw new Error("Function not implemented.");
+}
+
